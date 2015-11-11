b8f02e90ead8c7b3d5b10912e01f42bb6d57c911
satoaki131 <akinori_0131@yahoo.co.jp>
2015-11-12 00:42:38 +0900

    ストーンおけるようにした

--- Assets/Scripts/OthelloView.cs
+++ Assets/Scripts/OthelloView.cs
@@ -2,73 +2,79 @@
 
 public class OthelloView : MonoBehaviour
 {
-
     [SerializeField]
-    private GameObject[,] map = null;
+    private GameObject[,] _cells = null;
     private Renderer _renderer;
-
+  
     public int countX;
     public int countZ;
 
+    private const float CellMargin = 1.05f;
     private int selectX = 0;
     private int selectZ = 0;
-
+    
+ 
     void Start()
     {
-        map = new GameObject[countX, countZ];
+        _cells = new GameObject[countX, countZ];
         var cell = Resources.Load<GameObject>("Cell/Cell");
         for (int x = 0; x < countX; x++)
         {
             for (int z = 0; z < countZ; z++)
             {
-                map[x, z] = Instantiate(cell,
-                    new Vector3(x * 1.05f, 0.0f, z * 1.05f),
+                _cells[x, z] = Instantiate(cell,
+                    new Vector3(x * CellMargin, 0.0f, z * CellMargin),
                     new Quaternion(0.0f, 0.0f, 0.0f, 1.0f))
                     as GameObject;
 
-                map[x, z].transform.name =
+                _cells[x, z].transform.name =
                     "[" + x + " , " + z + " ] ";
 
-                map[x, z].transform.parent =
-                    this.gameObject.transform;
+                _cells[x, z].transform.parent =
+                    gameObject.transform;
 
                
             }
         }
-        _renderer = map[selectX, selectZ].GetComponent<Renderer>();
+        _renderer = _cells[selectX, selectZ].GetComponent<Renderer>();
         _renderer.material.color = Color.red;   
     }
 
     void Update()
     {
         Selectmap();
+        var stone = Resources.Load<GameObject>("Stone/Stone");
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            Instantiate(stone, new Vector3(_cells[selectX, selectZ].transform.position.x, 0.1f, _cells[selectX, selectZ].transform.position.z), transform.rotation);
+        }
     }
 
     void Selectmap()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            map[selectX, selectZ].GetComponent<Renderer>().material.color = Color.green;
+            _cells[selectX, selectZ].GetComponent<Renderer>().material.color = Color.green;
             if (selectZ < countZ - 1) selectZ += 1;
-            map[selectX, selectZ].GetComponent<Renderer>().material.color = Color.red;
+            _cells[selectX, selectZ].GetComponent<Renderer>().material.color = Color.red;
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            map[selectX, selectZ].GetComponent<Renderer>().material.color = Color.green;
+            _cells[selectX, selectZ].GetComponent<Renderer>().material.color = Color.green;
             if (selectZ > 0) selectZ -= 1;
-            map[selectX, selectZ].GetComponent<Renderer>().material.color = Color.red;
+            _cells[selectX, selectZ].GetComponent<Renderer>().material.color = Color.red;
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            map[selectX, selectZ].GetComponent<Renderer>().material.color = Color.green;
+            _cells[selectX, selectZ].GetComponent<Renderer>().material.color = Color.green;
             if (selectX < countX - 1) selectX += 1;
-            map[selectX, selectZ].GetComponent<Renderer>().material.color = Color.red;
+            _cells[selectX, selectZ].GetComponent<Renderer>().material.color = Color.red;
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            map[selectX, selectZ].GetComponent<Renderer>().material.color = Color.green;
+            _cells[selectX, selectZ].GetComponent<Renderer>().material.color = Color.green;
             if (selectX > 0) selectX -= 1;
-            map[selectX, selectZ].GetComponent<Renderer>().material.color = Color.red;
+            _cells[selectX, selectZ].GetComponent<Renderer>().material.color = Color.red;
         }
     }
   
